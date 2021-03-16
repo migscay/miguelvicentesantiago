56e7770d553cd57caa531571e4a9ec24eb154c84
Miguel Vicente O. Santiago <migscay@users.noreply.github.com>
2021-03-17 09:51:08 +1100

    particles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Navbar from "./components/Navbar";
+import Header from "./components/Header"
+import { Particles } from "react-particles-js";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900  
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {    
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }  
+        }}    
+      />
+      <Navbar />
+      <Header />
+    </>
   ) 
   
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Junior Web Developer</h1>
+                <Typed className="typed-text"
+                       strings={["Junior Web Developer","Javascript","HTML/CSS","Bootstrap","NPM","React","GIT/Github","PHP","C#","SQL"]}
+                       typeSpeed={80}
+                       backSpeed={80}
+                       loop
+                ></Typed>
+                <a href="#" className="btn-main-offer">Contact Me</a>
+            </div>
+        </div>  
+    )
+}
+
+export default Header
+
