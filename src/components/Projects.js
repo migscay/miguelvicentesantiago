75a24cc84060991a2c0a1d72bbab188c3e16b3f4
Miguel Vicente O. Santiago <migscay@users.noreply.github.com>
2021-03-19 12:58:53 +1100

    deploy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -32,10 +32,10 @@ const Projects = () => {
                         <p className="card-text projects__card__text">A project I did in the Codecademy JavaScript course. The only requirement was that the piano keys turn to blue on mousedown. I improved it by adding sound.</p>
                         <img className="card__img" src={piano_player} alt="piano-player"></img>
                         <hr/>
-                        <div class="pb-3">Languages: 
+                        {(<div class="pb-3">Languages: 
                             <p className="badge badge-light">JavaScript: 54.3 % HTML: 24.7 % CSS: 21 %
                             </p>
-                        </div>
+                        </div>)}
                     </div>
                 </div>
                 <div className="projects__card card shadow-lg p-3 mb-5 bg-white rounded card">
@@ -46,10 +46,10 @@ const Projects = () => {
                         <p className="card-text projects__card__text">A group project in the Generation Australia Bootcamp.</p>
                         <img className="card__img" src={task_planner} alt="task_planner"></img>
                         <hr/>
-                        <div class="pb-3">Languages: 
+                        {(<div class="pb-3">Languages: 
                             <p className="badge badge-light">JavaScript: 57.2 % HTML: 41.4 % CSS: 1.4 %
                             </p>
-                        </div>
+                        </div>)}
                     </div>
                 </div>
                 <div className="projects__card card shadow-lg p-3 mb-5 bg-white rounded card">
